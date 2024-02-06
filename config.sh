05754b1ae489da5e67a4bc84f91ff2c8f5616485
JoshKoiro <josh.koiro@gmail.com>
2024-02-06 09:36:30 -0500

    fixed #1 by adding colors to the CLI interface
    Added colors using the tput functions in bash

--- config.sh
+++ config.sh
@@ -1,59 +1,93 @@
 #!/bin/bash
 
-echo -e "\nThis script will walk you through the process of setting up Github authentication on your system using ssh.\n\n"
+# Color Definitions based on the numerical value used in the tput command
+SUCCESS=14
+ERROR=9
+INFO=5
+QUESTION=3
 
-read -p "Would you like to create a new key? (y/n)" newKey
+# Function to set colors in the script
+color() {
+	tput setaf $1
+	echo -e $2
+	tput sgr0
+}
 
+setColor() {
+	tput setaf $1
+}
+
+resetColor() {
+	tput sgr0
+}
+
+color 5 "\nThis script will walk you through the process of setting up Github authentication on your system using ssh.\n\n"
+
+setColor ${QUESTION}
+read -p "Would you like to create a new key? (y/n) " newKey
+resetColor
 if [ "$newKey" == "y" ]; then
-	echo -e "\nCreating new key...\n"
+	color ${INFO} "\nCreating new key...\n"
+	setColor ${QUESTION}
 	read -p "Please enter your email address that is associated with your Github account: " email
+	color ${INFO}
 	ssh-keygen -t rsa -b 4096 -C $email
+	resetColor
 elif [ "$newKey" == "n" ]; then
-	echo -e "\n\nNo new keys created..."
+	color ${INFO} "\nNo new keys created..."
 else
-	echo -e "Invalid data received. Please type y or n in prompt."
+	color ${ERROR} "\nInvalid data received. Please type y or n in prompt."
 	exit 1
 fi
 
 echo -e "\n"
 ls ~/.ssh
 echo -e "\n"
+setColor ${QUESTION}
 read -p "Above is the available keys generated on the system. Please type the name of the one created for Github: " authFile
-
-echo -e "\nBelow is your public key. Please copy this entirety of the contents below to enter on Github when you create the ssh key.\n"
+resetColor
+echo -e "\n"
+color ${SUCCESS} "Below is your public key. Please copy this entirety of the contents below to enter on Github when you create the ssh key.\n"
 
 cat ~/.ssh/$authFile.pub
 
 echo -e "\n"
-
+setColor ${QUESTION}
 read -p "Would you like to start ssh-keygen on login? (y/n) " startup
+resetColor
 echo -e "\n"
 
 if [ "$startup" == "y" ]; then
-	echo -e "\nAdding ssh-keygen to .bashrc file...\n"
+	color ${INFO} "\nAdding ssh-keygen to .bashrc file...\n"
+	setColor ${INFO}
 	echo "echo ssh-agent starting..." >>~/.bashrc
 	echo 'eval $(ssh-agent)' >>~/.bashrc
+	resetColor
+	setColor ${QUESTION}
 	read -p "would you like to automatically add your key to ssh on login? (y/n) " startupKey
+	resetColor
 	echo -e "\n"
 
 	if [ "$startup" == "y" ]; then
-		echo -e "\nAdding add-key command to .bashrc file...\n"
+		color ${INFO} "\nAdding add-key command to .bashrc file...\n"
+		setColor ${INFO}
 		echo "ssh-add ~/.ssh/${authFile}" >>~/.bashrc
+		resetColor
 	elif [ "$newKey" == "n" ]; then
-		echo -e "\nSkipping adding keys to startup...You will have to use ssh-add <key-filepath> manually on each login."
+		color ${INFO} "\nSkipping adding keys to startup...You will have to use ssh-add <key-filepath> manually on each login."
 	else
-		echo -e "Invalid data received. Please type y or n in prompt."
+		color ${ERROR} "Invalid data received. Please type y or n in prompt."
 		exit 1
 	fi
 
 elif [ "$newKey" == "n" ]; then
-	echo -e "\nSkipping adding ssh-keygen to startup..."
+	color ${INFO} "\nSkipping adding ssh-keygen to startup..."
 else
-	echo -e "Invalid data received. Please type y or n in prompt."
+	color ${ERROR} "Invalid data received. Please type y or n in prompt."
 	exit 1
 fi
 
-echo -e "Reloading .bashrc...\n"
+color ${INFO} "Reloading .bashrc...\n"
 source ~/.bashrc
-echo -e "Testing connection....\n"
+color ${INFO} "Testing connection....\n"
 ssh -T git@github.com
