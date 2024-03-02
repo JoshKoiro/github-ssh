1701b108c7461939a4f8e604504fba3ef38585e0
JoshKoiro <josh.koiro@gmail.com>
2024-03-02 15:31:20 -0500

    working on proper syntax of ssh-keygen - quotes wrong

--- config.bat
+++ config.bat
@@ -1,14 +1,23 @@
+@echo off
+setlocal
+
+
 echo This script will walk you through the process of setting up Github authentication on your system using ssh.
 echo.
 
-set /p newKey=Would you like to create a new key? (y/n) 
+:: Ensure the .ssh directory exists
+if not exist "%USERPROFILE%\.ssh\" mkdir "%USERPROFILE%\.ssh"
+
+echo %USERPROFILE%\.ssh\
+
+set /p newKey="Would you like to create a new key? (y/n) "
 if "%newKey%"=="y" (
     echo.
     echo Creating new key...
-    set /p email=Please enter your email address that is associated with your Github account: 
-    echo.
-    set /p keyName=Please enter the name of the key you would like to create (i.e. github): 
-    ssh-keygen -t rsa -b 4096 -C "%email%" -f %USERPROFILE%\.ssh\%keyName%
+    set /p email="Please enter your email address that is associated with your Github account: " 
+    set /p keyName="Please enter the name of the key you would like to create (i.e., github): "
+    echo `ssh-keygen -o -t rsa -C "%email%" -f "`%USERPROFILE%`\.ssh\`%keyName%"`
+    ssh-keygen -o -t rsa -C "%email%" -f "`%USERPROFILE%`\.ssh\`%keyName%"
 ) else if "%newKey%"=="n" (
     echo.
     echo No new keys created...
@@ -18,20 +27,25 @@ if "%newKey%"=="y" (
     exit /b 1
 )
 
-echo.
-dir %USERPROFILE%\.ssh
+echo
+cd "%USERPROFILE%\.ssh"
+dir
 echo.
 
-set /p authFile=Above is the available keys generated on the system. Please type the name of the one created for Github: 
-echo.
+set /p authFile="Above is the available keys generated on the system. Please type the name of the one created for Github: "
+echo
 echo Below is your public key. Please copy this entirety of the contents below to enter on Github when you create the ssh key.
+echo
+
+type "%authFile%.pub"
+
 echo.
+echo Press Enter to continue...
+pause >nul
 
-type %USERPROFILE%\.ssh\%authFile%.pub
+echo
 
-echo.
-
-:: Testing connection to GitHub via SSH
+:: No need to change back to the original directory since setlocal/endlocal encapsulates environment changes
 echo Testing connection....
 ssh -T git@github.com
 
